0b2ce80e723d84b40d7becaeb4aaaa3db1ccedf5
Mkeeper <794892050@qq.com>
2018-06-19 22:02:51 +0800

    chapter1-1-1

--- Chapter1-1-1/src/main/java/com/mkeeper/Application.java
+++ Chapter1-1-1/src/main/java/com/mkeeper/Application.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
     }
 }
