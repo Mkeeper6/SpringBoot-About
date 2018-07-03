4ca6383223aeb9ac3b163c2850fe22155f955548
Mkeeper <794892050@qq.com>
2018-07-03 11:47:58 +0800

    Chpater2-1-1: 搭建RESTful风格web服务

--- Chapter2-1-1/src/main/java/com/mkeeper/service/UserService.java
+++ Chapter2-1-1/src/main/java/com/mkeeper/service/UserService.java
@@ -1,19 +1,20 @@
 package com.mkeeper.service;
 
 import com.mkeeper.entity.User;
+
 import java.util.List;
 
 
 public interface UserService {
-     // 服务层：处理业务逻辑
+    // 服务层：处理业务逻辑
 
-     User findByUserId(Integer userId);
+    User findByUserId(Integer userId);
 
-     List<User> findList();
+    List<User> findList();
 
-     User addUser(User user);
+    User addUser(User user);
 
-     User updateUser(User user);
+    User updateUser(User user);
 
-     boolean deleteByUserId(Integer userId);
+    boolean deleteByUserId(Integer userId);
 }
