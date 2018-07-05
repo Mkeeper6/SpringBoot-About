edb7f95cac2d2e3b66e333a18f12b119525d5f34
Mkeeper <794892050@qq.com>
2018-07-05 10:54:31 +0800

    chapter2-1-3: 异常处理规范

---
+++ Chapter2-1-3/src/main/java/com/mkeeper/Chapter213Application.java
@@ -0,0 +1,11 @@
+package com.mkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter213Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter213Application.class, args);
+    }
+}
