f7ec0317b7062dbda60cb05ff999da188a7180ab
Mkeeper <794892050@qq.com>
2018-07-08 01:19:42 +0800

    Chapter2-1-3: 异常处理规范

--- Chapter2-1-3/src/main/java/com/mkeeper/Handler/GlobalExceptionHandler.java
+++ Chapter2-1-3/src/main/java/com/mkeeper/Handler/GlobalExceptionHandler.java
@@ -2,21 +2,19 @@ package com.mkeeper.Handler;
 
 import com.mkeeper.common.R;
 import com.mkeeper.exception.BusinessException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 // 如果要验证BadException 和 UglyException请注释@ControllerAdvice
- @ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseBody
     @ExceptionHandler(value = BusinessException.class)
     public R businessExceptionHandler(BusinessException exception) {
         return R.isFail(exception);
     }
 
-    @ResponseBody
     @ExceptionHandler(value = Exception.class)
     public R exceptionHandler(Exception exception) {
         return R.isFail(exception);
