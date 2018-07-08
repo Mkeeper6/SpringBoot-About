e5d49e3b7e940104bc637010023440474d0c3772
Mkeeper <794892050@qq.com>
2018-07-08 20:52:15 +0800

    Chapter2-1-5: 数据验证一

--- Chapter2-1-5/src/main/java/com/mkeeper/Chapter215Application.java
+++ Chapter2-1-5/src/main/java/com/mkeeper/Chapter215Application.java
@@ -3,6 +3,9 @@ package com.mkeeper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Mkeeper
+ */
 @SpringBootApplication
 public class Chapter215Application {
     public static void main(String[] args) {
