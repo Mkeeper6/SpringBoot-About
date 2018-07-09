3134b455ca85fcc35f89f13cd53a5385c04833e7
Mkeeper <794892050@qq.com>
2018-07-09 10:53:09 +0800

    Chapter2-1-5: 日志规范-logback配置详解

---
+++ Chapter2-1-5/src/main/java/com/mkeeper/controller/LogController.java
@@ -0,0 +1,18 @@
+package com.mkeeper.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogController {
+
+    @GetMapping("/log")
+    public void log(){
+        log.debug(this.getClass().getSimpleName() + ": debug");
+        log.info(this.getClass().getSimpleName() + ": info");
+        log.warn(this.getClass().getSimpleName() + ": warn");
+        log.error(this.getClass().getSimpleName() + ": error");
+    }
+}
