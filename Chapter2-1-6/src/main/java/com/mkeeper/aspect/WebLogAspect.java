c1311cf7aefcff62b5a37d4c5714d9619cbfb9b4
Mkeeper <794892050@qq.com>
2018-07-09 16:37:17 +0800

    Chapter2-1-6: 日志规范-使用AOP统一处理Web日志

---
+++ Chapter2-1-6/src/main/java/com/mkeeper/aspect/WebLogAspect.java
@@ -0,0 +1,46 @@
+package com.mkeeper.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class WebLogAspect {
+
+    @Pointcut("execution(public * com.mkeeper.controller..*.*(..))")
+    public void webLog(){}
+
+
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint){
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret){
+        // 处理完请求，返回内容
+        log.info("RESPONSE : " + ret);
+    }
+}
