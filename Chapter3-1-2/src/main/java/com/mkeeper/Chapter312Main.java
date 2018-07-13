b21cde1c806102913e0ea15e8719c860d20e8349
Mkeeper <794892050@qq.com>
2018-07-13 16:53:23 +0800

    Chapter3-1-2: Quartz定时任务整合

---
+++ Chapter3-1-2/src/main/java/com/mkeeper/Chapter312Main.java
@@ -0,0 +1,12 @@
+package com.mkeeper;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter312Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter312Main.class, args);
+    }
+}
