7b2fa8681baad796581afd24c936a2bbbc91480b
Mkeeper <794892050@qq.com>
2018-07-13 14:19:29 +0800

    Chapter2-1-10: 文件上传

--- Chapter2-2-10/src/main/java/com/mkeeper/controller/FileUploadController.java
+++ Chapter2-2-10/src/main/java/com/mkeeper/controller/FileUploadController.java
@@ -33,7 +33,7 @@ public class FileUploadController {
         log.info("[文件名称] - [{}]", file.getOriginalFilename());
         log.info("[文件大小] - [{}]", file.getSize());
         // TODO 将文件写入到指定目录（具体开发中有可能是将文件写入到云存储/或者指定目录通过 Nginx 进行 gzip 压缩和反向代理，此处只是为了演示故将地址写成本地电脑指定目录）
-        file.transferTo(new File("F:\\app\\chapter16\\" + file.getOriginalFilename()));
+        file.transferTo(new File("E:\\temp\\" + file.getOriginalFilename()));
         Map<String, String> result = new HashMap<>(16);
         result.put("contentType", file.getContentType());
         result.put("fileName", file.getOriginalFilename());
@@ -50,7 +50,7 @@ public class FileUploadController {
         List<Map<String, String>> results = new ArrayList<>();
         for (MultipartFile file : files) {
             // TODO Spring Mvc 提供的写入方式
-            file.transferTo(new File("F:\\app\\chapter16\\" + file.getOriginalFilename()));
+            file.transferTo(new File("E:\\temp\\" + file.getOriginalFilename()));
             Map<String, String> map = new HashMap<>(16);
             map.put("contentType", file.getContentType());
             map.put("fileName", file.getOriginalFilename());
@@ -64,7 +64,7 @@ public class FileUploadController {
     @ResponseBody
     public void upload2(String base64) throws IOException {
         // TODO BASE64 方式的 格式和名字需要自己控制（如 png 图片编码后前缀就会是 data:image/png;base64,）
-        final File tempFile = new File("F:\\app\\chapter16\\test.jpg");
+        final File tempFile = new File("E:\\temp\\test.jpg");
         // TODO 防止有的传了 data:image/png;base64, 有的没传的情况
         String[] d = base64.split("base64,");
         final byte[] bytes = Base64Utils.decodeFromString(d.length > 1 ? d[1] : d[0]);
