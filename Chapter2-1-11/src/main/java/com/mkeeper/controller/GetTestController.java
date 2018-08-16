c7d64b037488b38f9a95ab135db4ef3b4b5f5fca
Mkeeper <794892050@qq.com>
2018-08-16 17:31:51 +0800

    Chapter2-1-11: RestTemplate整合HttpClient访问Http接口

--- Chapter2-1-11/src/main/java/com/mkeeper/controller/GetTestController.java
+++ Chapter2-1-11/src/main/java/com/mkeeper/controller/GetTestController.java
@@ -1,6 +1,10 @@
 package com.mkeeper.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +21,30 @@ public class GetTestController {
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping("/baidu/{key}")
+    @GetMapping("/baidu1/{key}")
     public String get1(@PathVariable String key) throws UnsupportedEncodingException {
         String encodeKey = URLEncoder.encode(key, "UTF-8");
+
         String url = "http://www.baidu.com/s?bdorz_come=1&ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + encodeKey;
-        return restTemplate.getForObject(url, String.class);
+
+        return restTemplate.getForObject(url, String.class); //返回百度主站html
+    }
+
+    @GetMapping("/baidu2/{key}")
+    public String get2(@PathVariable String key) throws UnsupportedEncodingException {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.set("MyHeaderKey", "MyHeaderValue");
+
+        HttpEntity entity = new HttpEntity(headers);
+
+        String encodeKey =URLEncoder.encode(key, "UTF-8");
+
+        String url = "http://www.baidu.com/s?bdorz_come=1&ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + encodeKey;
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        return  response.getBody(); //返回百度主站html
     }
 
 }
