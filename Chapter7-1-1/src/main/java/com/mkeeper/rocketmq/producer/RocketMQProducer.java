554ce9c3805622bd4cd059aa4e1b4fd63372ba1f
Mkeeper <794892050@qq.com>
2018-09-14 11:16:27 +0800

    Chapter7-1-1:

--- Chapter7-1-1/src/main/java/com/mkeeper/rocketmq/producer/RocketMQProducer.java
+++ Chapter7-1-1/src/main/java/com/mkeeper/rocketmq/producer/RocketMQProducer.java
@@ -10,12 +10,9 @@ import org.apache.rocketmq.remoting.common.RemotingHelper;
 public class RocketMQProducer{
 
     public static void main(String[] args) throws MQClientException, InterruptedException {
-//        DefaultMQProducer producer = new DefaultMQProducer("producer1");
         DefaultMQProducer producer = new DefaultMQProducer("example_group_name");
 
         producer.setNamesrvAddr("192.168.162.129:9876");
-//        producer.setNamesrvAddr("118.24.172.27:9876");
-
 
         producer.start();
 
@@ -47,52 +44,3 @@ public class RocketMQProducer{
     }
 
 }
-
-
-
-/*
-@Component
-public class RocketMQProducer {
-    */
-/**
-     * 生产者的组名
-     *//*
-
-    @Value("${apache.rocketmq.producer.producerGroup}")
-    private String producerGroup;
-
-    */
-/**
-     * NameServer 地址
-     *//*
-
-    @Value("${apache.rocketmq.namesrvAddr}")
-    private String namesrvAddr;
-
-    @PostConstruct
-    public void defaultMQProducer() throws Exception{
-        //Instantiate with a producer group name.
-        MQProducer producer = new DefaultMQProducer("example_group_name");
-        //Launch the instance.
-        producer.start();
-        String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
-        for (int i = 0; i < 100; i++) {
-            int orderId = i % 10;
-            //Create a message instance, specifying topic, tag and message body.
-            Message msg = new Message("TopicTest", tags[i % tags.length], "KEY" + i,
-                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
-            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
-                @Override
-                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
-                    Integer id = (Integer) arg;
-                    int index = id % mqs.size();
-                    return mqs.get(index);
-                }
-            }, orderId);
-
-            System.out.printf("%s%n", sendResult);
-        }
-        //server shutdown
-        producer.shutdown();
-    }
-}*/
