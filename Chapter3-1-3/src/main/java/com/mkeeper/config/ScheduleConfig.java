0027fd9d0353f9d953c4a0b0be181e369ff9530c
Mkeeper <794892050@qq.com>
2018-10-19 10:01:50 +0800

    Chapter3-1-3: Quartz定时任务单点持久化

--- Chapter3-1-3/src/main/java/com/mkeeper/config/ScheduleConfig.java
+++ Chapter3-1-3/src/main/java/com/mkeeper/config/ScheduleConfig.java
@@ -18,14 +18,19 @@ public class ScheduleConfig {
     @Qualifier("scheduleBean")
     public SchedulerFactoryBean schedulerFactoryBean(@Qualifier("dataSource") DataSource dataSource) {
         SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        // 名称
         schedulerFactoryBean.setSchedulerName("TASK_EXECUTOR");
-        // 延迟10秒启动
+        // 延迟10秒启动Scheduler
         schedulerFactoryBean.setStartupDelay(10);
+        // 通过applicationContextSchedulerContextKey属性配置spring上下文
         schedulerFactoryBean.setApplicationContextSchedulerContextKey("applicationContextKey");
+        // 设置是否任意一个已定义的Job会覆盖现有的Job。默认为false，即已定义的Job不会覆盖现有的Job。
         schedulerFactoryBean.setOverwriteExistingJobs(true);
+        // 自动开始
         schedulerFactoryBean.setAutoStartup(true);
+        // 数据源
         schedulerFactoryBean.setDataSource(dataSource);
-        // 将 JobFactory 改为自定义的，否则在 Job 中注入 Bean 会失败
+        // 将JobFactory改为自定义的，否则在 Job 中注入 Bean 会失败
         schedulerFactoryBean.setJobFactory(scheduleJobFactory);
         return schedulerFactoryBean;
     }
