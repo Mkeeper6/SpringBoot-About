d70095a9f64a6c16e690f6bc0c53d8af00a2c823
Mkeeper <794892050@qq.com>
2018-10-23 10:15:30 +0800

    Chapter3-1-5: Elastic-Job快速整合

--- Chapter3-1-5/src/main/java/com/mkeeper/Chapter315Main.java
+++ Chapter3-1-5/src/main/java/com/mkeeper/Chapter315Main.java
@@ -1,11 +1,23 @@
 package com.mkeeper;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class Chapter315Main {
+public class Chapter315Main implements CommandLineRunner {
+
+    @Resource
+    private ElasticJobService elasticJobService;
+
     public static void main(String[] args) {
         SpringApplication.run(Chapter315Main.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        elasticJobService.scanAddJob();
     }
 }
