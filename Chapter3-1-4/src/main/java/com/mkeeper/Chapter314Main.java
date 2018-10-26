973aca14aa02d84b2ad30fc303d57e93cf18b66e
Mkeeper <794892050@qq.com>
2018-10-26 17:13:52 +0800

    Chapter3-1-4: xxl-job快速整合

--- Chapter3-1-4/src/main/java/com/mkeeper/Chapter314Main.java
+++ Chapter3-1-4/src/main/java/com/mkeeper/Chapter314Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Chapter314Main {
     public static void main(String[] args) {
-
         SpringApplication.run(Chapter314Main.class, args);
     }
 }
