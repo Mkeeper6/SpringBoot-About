b3f436ac654b78414695bf75eb9fed40153f002e
Mkeeper <794892050@qq.com>
2018-11-01 15:15:34 +0800

    Chapter3-1-5: Elastic-Job-lite快速整合

--- Chapter3-1-5/src/main/java/com/mkeeper/job/MyJob.java
+++ Chapter3-1-5/src/main/java/com/mkeeper/job/MyJob.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MyJob implements SimpleJob {
+    
     @Override
     public void execute(ShardingContext shardingContext) {
         //打印出任务相关信息，JobParameter用于传递任务的ID
