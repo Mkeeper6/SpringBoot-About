55f7d878c974ee5418de87860e360e145fb77517
Mkeeper <794892050@qq.com>
2018-11-19 17:22:27 +0800

    Chapter4-1-7: Sharding Jdbc分库配置

---
+++ Chapter4-1-7/src/main/java/com/mkeeper/mapper/StockMapper.java
@@ -0,0 +1,17 @@
+package com.mkeeper.mapper;
+
+import com.mkeeper.entity.Stock;
+
+public interface StockMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Stock record);
+
+    int insertSelective(Stock record);
+
+    Stock selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Stock record);
+
+    int updateByPrimaryKey(Stock record);
+}
--- Chapter4-1-7/src/main/java/com/mkeeper/service/DemoService.java
+++ Chapter4-1-7/src/main/java/com/mkeeper/service/DemoService.java
@@ -2,13 +2,13 @@ package com.mkeeper.service;
 
 import com.mkeeper.entity.Order;
 import com.mkeeper.entity.OrderItem;
+import com.mkeeper.entity.Stock;
 import com.mkeeper.mapper.OrderItemMapper;
 import com.mkeeper.mapper.OrderMapper;
+import com.mkeeper.mapper.StockMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class DemoService {
@@ -18,35 +18,29 @@ public class DemoService {
     
     @Resource
     private OrderItemMapper orderItemMapper;
-    
+
+    @Resource
+    private StockMapper stockMapper;
+
     public void demo() {
-        orderMapper.createIfNotExistsTable();
-        orderItemMapper.createIfNotExistsTable();
-        orderMapper.truncateTable();
-        orderItemMapper.truncateTable();
-        List<Long> orderIds = new ArrayList<>(10);
-        System.out.println("1.Insert--------------");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
+
+            Stock stock = new Stock();
+            stock.setId(i);
+            stock.setName("name " + i);
+            stockMapper.insert(stock);
+
             Order order = new Order();
             order.setUserId(51 + i);
             order.setStatus("INSERT_TEST");
             orderMapper.insert(order);
             long orderId = order.getOrderId();
-            orderIds.add(orderId);
-            
+
             OrderItem item = new OrderItem();
             item.setOrderId(orderId);
             item.setUserId(51 + i);
             orderItemMapper.insert(item);
         }
         System.out.println(orderItemMapper.selectAll());
-        /*System.out.println("2.Delete--------------");
-        for (Long each : orderIds) {
-            orderRepository.delete(each);
-            orderItemRepository.delete(each);
-        }
-        System.out.println(orderItemRepository.selectAll());
-        orderItemRepository.dropTable();
-        orderRepository.dropTable();*/
     }
 }
