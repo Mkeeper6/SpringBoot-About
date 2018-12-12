fbea4dde3f9b5be7a85cd6a15ec352e487df2553
Mkeeper <794892050@qq.com>
2018-12-12 15:23:21 +0800

    Chapter4-1-8: Sharding Jdbc分库分表配置

--- Chapter4-1-8/src/main/java/com/mkeeper/service/DemoService.java
+++ Chapter4-1-8/src/main/java/com/mkeeper/service/DemoService.java
@@ -19,45 +19,50 @@ package com.mkeeper.service;
 
 import com.mkeeper.entity.Order;
 import com.mkeeper.entity.OrderItem;
-import com.mkeeper.entity.Stock;
 import com.mkeeper.mapper.OrderItemMapper;
 import com.mkeeper.mapper.OrderMapper;
-import com.mkeeper.mapper.StockMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
 public class DemoService {
-
     @Resource
     private OrderMapper orderMapper;
 
     @Resource
     private OrderItemMapper orderItemMapper;
 
-    @Resource
-    private StockMapper stockMapper;
-
     public void demo() {
         for (int i = 1; i <= 10; i++) {
-
-            Stock stock = new Stock();
-            stock.setId(i);
-            stock.setName("name " + i);
-            stockMapper.insert(stock);
-
             Order order = new Order();
-            order.setUserId(51 + i);
+            order.setUserId(1);
+            order.setOrderId(51 + i);
             order.setStatus("INSERT_TEST");
             orderMapper.insert(order);
             long orderId = order.getOrderId();
 
             OrderItem item = new OrderItem();
+            item.setOrderItemId(51 + i);
             item.setOrderId(orderId);
-            item.setUserId(51 + i);
+            item.setUserId(1);
             orderItemMapper.insert(item);
         }
-        System.out.println(orderItemMapper.selectAll());
+
+        for (int i = 1; i <= 10; i++) {
+            Order order = new Order();
+            order.setUserId(2);
+            order.setOrderId(100 + i);
+            order.setStatus("INSERT_TEST");
+            orderMapper.insert(order);
+            long orderId = order.getOrderId();
+
+            OrderItem item = new OrderItem();
+            item.setOrderItemId(100 + i);
+            item.setOrderId(orderId);
+            item.setUserId(2);
+            orderItemMapper.insert(item);
+        }
+        System.out.println(orderItemMapper.selectAll().size());
     }
 }
