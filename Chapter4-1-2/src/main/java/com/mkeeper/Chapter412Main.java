bfc0006d23a4be172ee145e850747295d24dc30b
Mkeeper <794892050@qq.com>
2018-12-18 16:51:00 +0800

    Chapter4-1-2: 使用Mybatis操作数据库

--- Chapter4-1-2/src/main/java/com/mkeeper/Chapter412Main.java
+++ Chapter4-1-2/src/main/java/com/mkeeper/Chapter412Main.java
@@ -2,6 +2,7 @@ package com.mkeeper;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +13,7 @@ public class Chapter412Main {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Chapter412Main.class);
-        //app.setBannerMode(Banner.Mode.OFF);
+        app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
         log.info("Chapter4-1-2 is success!");
     }
