4461780a396304582a28f15151ade5560e349630
Mkeeper <794892050@qq.com>
2019-01-02 19:44:26 +0800

    Chapter4-1-2: 使用Mybatis操作数据库

--- Chapter4-1-2/src/main/java/com/mkeeper/entity/User.java
+++ Chapter4-1-2/src/main/java/com/mkeeper/entity/User.java
@@ -5,6 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+/**
+ * CREATE TABLE `t_user` (
+ *   `user_id` int(11) NOT NULL AUTO_INCREMENT,
+ *   `user_name` varchar(255) NOT NULL,
+ *   `password` varchar(255) NOT NULL,
+ *   `phone` varchar(255) NOT NULL,
+ *   PRIMARY KEY (`user_id`)
+ * ) ENGINE=InnoDB AUTO_INCREMENT=1088 DEFAULT CHARSET=utf8;
+ */
+
+
 @Data
 @Builder
 @NoArgsConstructor
