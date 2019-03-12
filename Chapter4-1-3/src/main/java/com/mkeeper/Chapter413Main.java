25d0206881ca5a5fb80cfe47b6e6038f86bcc087
Mkeeper <794892050@qq.com>
2019-03-12 20:19:31 +0800

    Chapter4-1-3 使用Mybatis-Plus操作数据库

--- Chapter4-1-3/src/main/java/com/mkeeper/Chapter413Main.java
+++ Chapter4-1-3/src/main/java/com/mkeeper/Chapter413Main.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @SpringBootApplication
 public class Chapter413Main {
-
     protected final static Logger logger = LoggerFactory.getLogger(Chapter413Main.class);
 
     /**
